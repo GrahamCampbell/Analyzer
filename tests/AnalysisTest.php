c7fb3db2be99a6201a25281e82df0050d68af824
Graham Campbell <graham@alt-three.com>
2017-07-28 13:00:40 +0100

    Added analysis test

---
+++ tests/AnalysisTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of Analyzer.
+ *
+ * (c) Graham Campbell <graham@alt-three.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace GrahamCampbell\Tests\Analyzer;
+
+use GrahamCampbell\Analyzer\AnalysisTrait;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * This is the analysis test class.
+ *
+ * @author Graham Campbell <graham@alt-three.com>
+ */
+class AnalysisTest extends TestCase
+{
+    use AnalysisTrait;
+
+    protected function getPaths()
+    {
+        return [
+            realpath(__DIR__.'/../src'),
+            realpath(__DIR__),
+        ];
+    }
+}
